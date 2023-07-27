3e6defa88a674638b219d3f30398428408a74f3f
TECQNIO <TECQNIO@DESKTOP-CCNI10G>
2023-07-27 16:21:25 +0530

    commit1

---
+++ src/main/java/com/LandsandHomes/GenericUtility/WebDriverUtility.java
@@ -0,0 +1,5 @@
+package com.LandsandHomes.GenericUtility;
+
+public class WebDriverUtility {
+
+}
---
+++ src/main/java/com/LandsandHomes/PomRepository/LandsandHomesAddProperty.java
@@ -0,0 +1,5 @@
+package com.LandsandHomes.PomRepository;
+
+public class LandsandHomesAddProperty {
+
+}
